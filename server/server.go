8be78aa9099a508c1914b1040a6a8727d13a98d2
Surabodee <surabodee.sitc@gmail.com>
2026-03-28 11:40:11 +0700

    add Assignment gRPC

---
+++ server/server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "grpc-student/studentpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedStudentServiceServer
+}
+
+func (s *server) GetStudent(ctx context.Context, req *pb.StudentRequest) (*pb.StudentResponse, error) {
+
+	log.Printf("Received request for student ID: %d", req.Id)
+
+	// Mock data
+	return &pb.StudentResponse{
+		Id:    req.Id,
+		Name:  "Surabodee Phasuk",
+		Major: "Computer Science",
+		Email: "surabodee.pha@dome.tu.ac.th",
+		Phone: "091-558-9501",
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterStudentServiceServer(grpcServer, &server{})
+
+	log.Println("gRPC Server running on port 50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
+func (s *server) ListStudents(ctx context.Context, req *pb.Empty) (*pb.StudentListResponse, error) {
+    studentList := []*pb.StudentResponse{
+        {Id: 101, Name: "Alice Johnson", Major: "CS", Email: "alice@tu.ac.th", Phone: "091-558-9501"},
+        {Id: 102, Name: "Bob Smith", Major: "IT", Email: "bob@tu.ac.th",  Phone: "091-558-9502"},
+    }
+    return &pb.StudentListResponse{Student: studentList}, nil
+}
